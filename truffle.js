baa3981616b18e2050fe50b354723ef87c76f1b6
Robby Greenfield <robertgreenv@gmail.com>
2018-05-18 07:41:40 -0700

    adding comments to put your own wallet info
    adding comments to put your own wallet info

--- truffle.js
+++ truffle.js
@@ -1,11 +1,11 @@
 //const HDWalletProvider = require("truffle-hdwallet-provider")
-//const mnemonic = "solution limit tip risk custom soft cement fatigue accuse deputy youth hammer"
+//const mnemonic = "[YOUR MNEMONIC]"
 //to use pk and not mnemonic
 var PrivateKeyProvider = require("truffle-privatekey-provider");
-const privKey = "9743959134711147C2D4E18B0CDDC80FB33EEB97D137E9E37C5A68874F2B9D74"
+const privKey = "[YOUR PRIVATE KEY]"
 
 //latest deployed contract address: 0xdac2ff7c887cd14cde7e6e8411f6b40eba1f3cc1
-//on rinkeby 
+//on rinkeby
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -16,8 +16,8 @@ module.exports = {
     },
     rinkeby: {
       provider: function() {
-        //return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/CnZO2MtdzUwRXRDl941y")
-        return new PrivateKeyProvider(privKey, 'https://rinkeby.infura.io/CnZO2MtdzUwRXRDl941y')
+        //return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/[YOUR INFURA TOKEN HERE]")
+        return new PrivateKeyProvider(privKey, 'https://rinkeby.infura.io/[YOUR INFURA TOKEN HERE]')
       },
       network_id: '4'
     }
