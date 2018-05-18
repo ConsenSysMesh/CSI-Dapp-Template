d126e04b242251df2dacc0d02e9eb8cbb334b924
Robby Greenfield <robertgreenv@gmail.com>
2018-05-18 07:40:16 -0700

    add comments to help learners

--- src/layouts/views/home/HomeContainer.js
+++ src/layouts/views/home/HomeContainer.js
@@ -2,6 +2,7 @@ import Home from './Home'
 import { drizzleConnect } from 'drizzle-react'
 
 // May still need this even with data function to refresh component on updates for this contract.
+//for more on what the hell this is, check out here: https://github.com/trufflesuite/drizzle
 const mapStateToProps = state => {
   return {
     accounts: state.accounts,
